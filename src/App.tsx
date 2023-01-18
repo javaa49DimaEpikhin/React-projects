c4e838e69d63dbd9a820d63487209386a3300f49
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-18 16:00:03 +0200

    HW #33 Timer React

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import { Timer } from './components/Timer';
 
 
 function App() {
-  return <div>
-    <Timer timeZoneIndex={20}></Timer>
-    <Timer timeZoneIndex={5}></Timer>
+  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
+  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
+  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
+  return <div style={flexColumn}>
+    <div style={flexRow}>
+      <Timer cityOrCountry="London"></Timer>
+      <Timer cityOrCountry="Paris"></Timer>
+    </div>
+    <div style={flexRow}>
+      <Timer cityOrCountry="India "></Timer>
+      <Timer cityOrCountry={"Cuba"}></Timer>
+    </div>
+
   </div>
-    
+
 }
 
 export default App;
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,12 +1,14 @@
-import { BlockList } from "net";
+
 import React from "react";
 
 import timeZones from "../time-zones";
 type TimerProps = {
-    timeZoneIndex: number;
+    cityOrCountry: string;
 }
 export const Timer: React.FC<TimerProps> = (props) => {
-    const timeZone = timeZones[props.timeZoneIndex].name;
+    const timeZoneIndex: number = timeZones.findIndex(tz => JSON.stringify(tz).includes(props.cityOrCountry))
+    const timeZone:string = timeZones[timeZoneIndex]?.name;
+    const timeZoneName: string = timeZone ? props.cityOrCountry : "Israel"
     const [time, setTime] = React.useState(new Date());
     function tick() {
         console.log("tick");
@@ -18,8 +20,8 @@ export const Timer: React.FC<TimerProps> = (props) => {
     }, [])
     
     return <div>
-        <h3>Time in time zone {timeZone}</h3>
+        <h3 className='logo'>Time in {timeZoneName}</h3>
         <label style={{display: "block",
-         textAlign: "center", fontSize: "2em"}}>Time {time.toLocaleTimeString(undefined,{timeZone})}</label>
+         textAlign: "center", fontSize: "2em"}}>{time.toLocaleTimeString(undefined,{timeZone})}</label>
     </div>
 }
