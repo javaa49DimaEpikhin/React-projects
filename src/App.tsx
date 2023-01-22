e0ba6a33e1d8cc90d1e85ccab256a50d0eb1fda2
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-22 20:06:17 +0200

    HW #34 Timer With Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Input } from './components/Input';
-
+import { Timer } from './components/Timer';
 
 
 function App() {
-  function processInput(value: string): string {
-    let res: string = '';
-    if(value.toLowerCase().includes("hello")) {
-        res = "get tired from HELLO. It's forbidden word";
-    }
-    return res;
-    
-  }
-  return <Input inputId={'input-1'} inputProcess={processInput} />
+  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
+  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
+  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
+  return <div style={flexColumn}>
+    <div style={flexRow}>
+      <Timer cityOrCountry="London"></Timer>
+      <Timer cityOrCountry="Paris"></Timer>
+    </div>
+    <div style={flexRow}>
+      <Timer cityOrCountry="India "></Timer>
+      <Timer cityOrCountry={"Cuba"}></Timer>
+    </div>
+
+  </div>
 
 }
 
