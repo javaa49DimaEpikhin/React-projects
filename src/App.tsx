49e4adeca39752f11ed5902e656a447841626266
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-24 17:49:55 +0200

    last CW commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { getByDisplayValue } from '@testing-library/react';
-import React from 'react';
 import { Input } from './components/Input';
+import React from 'react';
 
 
 
@@ -9,23 +8,24 @@ function App() {
     display: "flex",
     flexWrap: "wrap"
   }
-  const [colors, setColors] = React.useState<string[]>([])
-  function creatingDivs(value: string): string {
-    const colors: string[] = value.split("#"); //red#green => ["red", "green"]
-    setColors(colors.slice());
+  const [colors,setcolors] = React.useState<string[]>([])
+function creatingDivs(value:string): string {
+    const colors: string[] = value.split("#") ;// red#green => ['red,'green']
+    setcolors(colors.slice());
     return '';
-
-  }
-  function getDivs(colors: string[]): JSX.Element[] {
-    return colors.map(color => <div style={{width: "5vw", height: "5vw",
-     backgroundColor: color }}></div>)
-  }
-  return <section style = {{display: "flex", flexDirection:"column"}}>
-    <Input placeHolder={'enter colors separated by #'} inputProcess={creatingDivs}></Input>
-    <section style={properties}>
-        {getDivs(colors)}
-    </section>
-  </section>
+}
+function getDivs(colors: string[]): JSX.Element[]{
+  return colors.map(color => <div style={{width:"5vw" , height:"5vh",
+backgroundColor:color}}></div>)
 }
 
+return <section style={{display: "flex",flexDirection:"column"}}>
+   <Input placeHolder={'enter color separated by #'} inputProcess={creatingDivs}/> 
+    <section style={properties}>
+   {getDivs(colors)}
+    </section>
+
+  </section>
+
+}
 export default App;
